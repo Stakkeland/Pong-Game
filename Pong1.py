09eb00a779df6f7f65f460b589e1bec4a3f395d9
Stakkeland <sawyerstakkeland@gmail.com>
2024-05-31 16:18:56 -0700

    first commit

---
+++ Pong1.py
@@ -0,0 +1,128 @@
+import turtle
+import pygame
+
+wn = turtle.Screen()
+wn.title('Pong by Sauce')
+wn.bgcolor('black')
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+player_a_name = input('What is your name player 1? ')
+player_a_color = input('What is your favorite color player 1? ')
+player_b_name = input('What is your name player 2? ')
+player_b_color = input('What is your favorite color player 2? ')
+
+#score
+score_a = 0
+score_b = 0
+
+#paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color(player_a_color)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color(player_b_color)
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('square')
+ball.color('white')
+ball.penup()
+ball.goto(0, 0)
+#maybe play with these 2 numbers maybe less to 1.5 or soemthing
+ball.dx = .2
+ball.dy = -.2
+
+#pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player A: 0   Player B: 0', align='center', font=('courier', 24, 'normal'))
+
+#Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y) 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y) 
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+#keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# Theme music
+pygame.mixer.init()
+sound = pygame.mixer.Sound('theme.wav')
+sound.play()
+
+#main game loop
+while True:
+    wn.update()
+
+    #move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f'{player_a_name}: {score_a}   {player_b_name}: {score_b}'.format(score_a, score_b), align='center', font=('courier', 24, 'normal'))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f'{player_a_name}: {score_a}   {player_b_name}: {score_b}'.format(score_a, score_b), align='center', font=('courier', 24, 'normal'))
+
+    #paddle collision
+    if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+        pygame.mixer.music.load('pong hit.mp3')
+        pygame.mixer.music.play()
+
+    if ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1
+        pygame.mixer.music.load('pong hit.mp3')
+        pygame.mixer.music.play()
---
+++ README.md
@@ -0,0 +1,25 @@
+# Overview
+
+This is a basic pong game with user customization.
+
+This game keeps track of the score and lets you play until you quit. It is a 2 player game with customizable name and paddle color. Press W and S for Player 1 and the up and down key for Player 2 to move the paddle.
+
+I wanted a small project to learn how to use python turtle and pygame.
+
+[Software Demo Video](https://youtu.be/Sl2C7Nfx0ro)
+
+# Development Environment
+
+Turtle, Pygame, Python, Visual Studio Code, Github
+
+# Useful Websites
+
+* [docs.python](https://docs.python.org/3/library/turtle.html)
+* [w3schools](https://www.w3schools.com/python/)
+* [pygame](https://www.pygame.org/docs/ref/mixer.html)
+
+# Future Work
+
+* Add highest score graphic.
+* ALlow user change speed of the game.
+* Allow user to change paddle size.
